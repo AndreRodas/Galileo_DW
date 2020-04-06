dc39f381a0be683a90b205bd5c6e3904184463fe
AndreRodas <andrejosue.rh@gmail.com>
2020-04-05 19:14:24 -0600

    ejercicio4final

--- Lab4/lab4_ejercicio4.sql
+++ Lab4/lab4_ejercicio4.sql
@@ -1,9 +1,10 @@
 -- Autor: André Rodas
 
-CREATE OR REPLACE FUNCTION FORMATO_TELEFONO(num text) RETURNS text AS $$
+DROP FUNCTION FORMATO_TELEFONO(num text);
+CREATE FUNCTION FORMATO_TELEFONO(num text) RETURNS text AS $$
 BEGIN
-	RETURN regexp_replace(num, '(\d{3})(\d{4})(\d{4})', '+(\1)(\3)-(\3)');
+	RETURN regexp_replace(num, '(\d{3})(\d{4})(\d{4})', '+(\1) \3-\3');
 END; $$ LANGUAGE plpgsql;
 
-
+SELECT FORMATO_TELEFONO('50211111111');
 
